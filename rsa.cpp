db83d161775ee003ba18e3b9fe0506a0bd9767d6
Mayank Kush <mayankkush1734@gmail.com>
2024-11-15 19:32:00 +0530

    rsa.cpp

--- rsa.cpp
+++ rsa.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <string>
-#include <cmath>
 #include <vector>
 using namespace std;
 
@@ -13,43 +12,56 @@ int gcd(int a, int b) {
     return a;
 }
 
+int modInverse(int a, int mod) {
+    int m0 = mod, y = 0, x = 1;
+    while (a > 1) {
+        int q = a / mod;
+        int t = mod;
+        mod = a % mod, a = t;
+        t = y;
+        y = x - q * y;
+        x = t;
+    }
+    if (x < 0)
+        x += m0;
+    return x;
+}
+
 int modExp(int base, int exp, int mod) {
     int result = 1;
     base = base % mod;
     while (exp > 0) {
-        if (exp % 2 == 1) {
+        if (exp % 2 == 1)
             result = (result * base) % mod;
-        }
         exp = exp >> 1;
         base = (base * base) % mod;
     }
     return result;
 }
 
-class RSAKeys { 
+class RSAKeys {
 public:
     int e, d, n;
-    RSAKeys(int p, int q) { 
+    RSAKeys(int p, int q) {
         n = p * q;
-        int Nn = (p - 1) * (q - 1);
+        int phi_n = (p - 1) * (q - 1);
         e = -1;
-        for (int i = 2; i < 100; i++) {
-            if (gcd(Nn, i) == 1) {
+        for (int i = 2; i < phi_n; i++) {
+            if (gcd(phi_n, i) == 1) {
                 e = i;
                 break;
             }
         }
         if (e != -1) {
-
-            d = modExp(e, -1, Nn);
+            d = modInverse(e, phi_n);
         } else {
-            cout << "Failed to find a suitable e." << endl;
-            d = -1;
+            cout << "Failed to find a suitable 'e'. Exiting..." << endl;
+            exit(1);
         }
     }
 };
 
-vector<int> convertMessageToArray(const string& message) {
+vector<int> convertMessageToArray(const string &message) {
     vector<int> result;
     for (char c : message) {
         result.push_back(static_cast<int>(c));
@@ -57,31 +69,48 @@ vector<int> convertMessageToArray(const string& message) {
     return result;
 }
 
+string convertArrayToMessage(const vector<int> &array) {
+    string result;
+    for (int i : array) {
+        result += static_cast<char>(i);
+    }
+    return result;
+}
+
 int main() {
     int p, q;
     string userInput;
-    vector<int> CipherText;
+    vector<int> cipherText;
+
     cout << "Enter two prime numbers to generate the keys: ";
     cin >> p >> q;
-    cin.ignore();                                                       // Ignore the newline character left in the input buffer
-    RSAKeys keys(p, q); 
-    cout << "Your private key is: {" << keys.e << ", " << keys.n << "}" << endl;
-    cout << "Your public key is: {" << keys.d << ", " << keys.n << "}" << endl;
+    cin.ignore();
+
+    RSAKeys keys(p, q);
+    cout << "Your public key is: {" << keys.e << ", " << keys.n << "}" << endl;
+    cout << "Your private key is: {" << keys.d << ", " << keys.n << "}" << endl;
+
     cout << "Enter the message you want to encrypt: ";
     getline(cin, userInput);
-    vector<int> pu = convertMessageToArray(userInput);
-     for(int i : pu){
-        cout<<i<<" ";
+
+    vector<int> plainText = convertMessageToArray(userInput);
+
+    for (int i : plainText) {
+        cipherText.push_back(modExp(i, keys.e, keys.n));
     }
-    for(int i : pu){
-        CipherText.push_back(modExp(i, keys.e, keys.n));                // Append each ciphertext to CipherText vector
-    } 
-        cout<<endl;
-        cout<<"your encrypted message is :";
-    for(int i : CipherText){
-        cout<<i<<" ";
+
+    cout << "Your encrypted message is: ";
+    for (int i : cipherText) {
+        cout << i << " ";
     }
+    cout << endl;
+
+    vector<int> decryptedText;
+    for (int i : cipherText) {
+        decryptedText.push_back(modExp(i, keys.d, keys.n));
+    }
+
+    cout << "Your decrypted message is: " << convertArrayToMessage(decryptedText) << endl;
+
     return 0;
 }
-
-
